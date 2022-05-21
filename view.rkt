22ec2ad5983d6490be8aaba38312653e97715e66
btyler_biscit <email.ben.tyler@gmail.com>
2022-05-21 17:21:56 +1000

    add in view, get states changing

---
+++ view.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+(require racket/gui/easy
+         racket/gui/easy/operator)
+
+(define @todos (@ '(
+                    (0 . "Get nothing done" . not-done)
+                    (1 . "Get things done" . not-done))))
+
+(define (mark-as-done key-state)
+  (list (car key-state) 'done))
+ 
+(define (append-todo todos)
+  (define next-id (add1 (apply max (map cadr todos))))
+  (append todos `((,next-id . "one more thing" . not-done))))
+ 
+(define (update-todo todos k proc)
+  (for/list ([entry (in-list todos)])
+    (if (eq? (car entry) k)
+        (cons k (proc (cdr entry)))
+        entry)))
+ 
+(define (todo @todo action)
+  (hpanel
+   #:stretch '(#t #f)
+   (text (obs-peek @todo))
+   (button "λ" (λ () (action mark-as-done)))))
+ 
+(render
+ (window
+  #:size '(#f 200)
+  (vpanel
+   (hpanel
+    #:alignment '(center top)
+    #:stretch '(#t #f)
+    (button "Add todo" (λ () (@todos . <~ . append-todo))))
+   (list-view
+    @todos
+    #:key car
+    (λ (k @entry)
+      (todo
+       (@entry . ~> . car)
+       (λ (proc)
+         (@todos . <~ . (λ (todos) (update-todo todos k proc))))))))))
